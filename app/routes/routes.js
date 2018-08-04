4383a94bef3f676924b3fc3b75821b833d483c0c
Wiktor Flis <wikflis@gmail.com>
2018-08-04 11:45:03 +0200

    working omdb api calls

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,11 @@
 const movieSchema = require('./validation/movies/schema.js');
 const commentSchema = require('./validation/comments/schema.js');
 const Joi = require('joi');
+const request = require('request-promise');
+
+const OMDB_APIADDRESS = 'http://www.omdbapi.com/';
+const OMDB_APIKEYSTRING = '&apikey=ca4f39b1';
+
 module.exports = function(app, db) {
     
     // fetches more info for a given movie, and saves it into app database
@@ -8,15 +13,27 @@ module.exports = function(app, db) {
         console.log("POST request body : " + req.body.title)
         movieSchema.postMovies.validate(req.body, {abortEarly: false})
             .then(validRequest => {
-                console.log("Valid request!")
-
-                res.status(200).send();
-            })
-            .catch(validationError => {
+                console.log("Valid request!");
+                
+                const moviestring = req.body.title;
+                console.log(moviestring);
+                return moviestring;
+            }).catch(validationError => {
                 console.log("Invalid request!")
 
-                const err = validationError.details.map(d => d.message);
-                res.status(400).send(err);
+                //const err = validationError.details.map(d => d.message);
+                res.status(400).send();
+            })
+            .then(moviestring => {
+                moviestring = moviestring.replace(' ', '+');
+                //omdb api call
+                var url = OMDB_APIADDRESS + '?t=' + moviestring + OMDB_APIKEYSTRING;
+                console.log("GET " + url);
+                request(url).then( (data) => {
+                        console.log(data);
+                        res.status(202).send(data);
+
+                });
             })
     });
     // fetches list of all movies already present in app database
